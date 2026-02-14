918bfc1b65d95c9e75251aadbfe479cdc8ab2a6f
MKCS2299 <98272921+MKCS2299@users.noreply.github.com>
2026-02-14 19:49:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    const introOverlay = document.getElementById("netflixIntro");
     const startBtn = document.getElementById("startBtn");
     const bgMusic = document.getElementById("bgMusic");
-    const clap = document.getElementById("clapSound");
 
     const sections = ["s1", "s2", "s3", "s4"];
     let current = 0;
 
+    // Remove intro overlay fully after animation
+    setTimeout(() => {
+        introOverlay.style.display = "none";
+    }, 3000);
+
     startBtn.addEventListener("click", function () {
-        bgMusic.play().catch(() => {});
         document.getElementById("intro").style.display = "none";
+
+        if (bgMusic) {
+            bgMusic.play().catch(() => {});
+        }
+
         showSection(current);
     });
 
     document.querySelectorAll(".nextBtn").forEach(btn => {
         btn.addEventListener("click", function () {
-            clap.play();
             current++;
             if (current < sections.length) {
                 showSection(current);
